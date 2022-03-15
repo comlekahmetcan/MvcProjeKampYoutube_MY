46bb20f747f6db70b1af21bdfc0c5b4ccc47e5f2
comlek <comlekahmetcan97@hotmail.com>
2022-03-15 12:35:40 +0300

    FluentValidation(CategoryValidation oluşturuldu-30)

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -24,5 +24,6 @@ namespace BusinessLayer.Concrete
         {
             return _categoryDAL.List();
         }
+
     }
 }
---
+++ BusinessLayer/ValidationRules/CategoryValidatior.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidatior : AbstractValidator<Category>
+    {
+        //nugetden businesslayer katmanına fluentvalidation paketini kurduk
+        //doğrulama kuralları ctor içine yazılır
+        public CategoryValidatior()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori Adını Boş Geçemezsiniz..!");//doğrulama kuralları
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Açıklamayı Boş Geçemezsiniz..!");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Lütfen En az 3 karakter girişi yapınız...!");
+            RuleFor(x => x.CategoryName).MaximumLength(20).WithMessage("Lütfen 20 karakterden fazla değer girişi yapmayın...!");
+        }
+    }
+}
