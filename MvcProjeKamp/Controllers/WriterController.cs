c171ba21dbf978d8cea4203a1c5310942570c451
comlekahmetcan <77794522+comlekahmetcan@users.noreply.github.com>
2022-03-29 13:46:25 +0300

    Add Migration(Writer) - Yazar İşlemleri-44

--- MvcProjeKamp/Controllers/WriterController.cs
+++ MvcProjeKamp/Controllers/WriterController.cs
@@ -1,5 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +18,30 @@ namespace MvcProjeKamp.Controllers
         {
             var WriterValues = wm.GetList();
             return View(WriterValues);
+        }
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddWriter(Writer p)
+        {
+            WriterValidatior writervalidatior = new WriterValidatior();
+            ValidationResult results = writervalidatior.Validate(p);
+            if(results.IsValid)
+            {
+                wm.WriterAdd(p);
+                RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
 
     }
