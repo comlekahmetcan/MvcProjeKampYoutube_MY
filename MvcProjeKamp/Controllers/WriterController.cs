04fb5687f7c13ad0b830a11fbb7f01a8208ce7e5
comlekahmetcan <77794522+comlekahmetcan@users.noreply.github.com>
2022-03-29 12:10:17 +0300

    Yazar işlemleri(42)

---
+++ MvcProjeKamp/Controllers/WriterController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKamp.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterDAL());
+        public ActionResult Index()
+        {
+            var WriterValues = wm.GetList();
+            return View(WriterValues);
+        }
+
+    }
+}
