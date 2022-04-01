2fe969b6052b64967b0112e0cc99a811ba70369b
comlekahmetcan <77794522+comlekahmetcan@users.noreply.github.com>
2022-04-01 12:17:51 +0300

    Başlık işlemleri-Genel yapı oluşturuldu(46)

---
+++ MvcProjeKamp/Controllers/HeadingController.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKamp.Controllers
+{
+    public class HeadingController : Controller
+    {
+        // GET: Heading
+        HeadingManager hm = new HeadingManager(new EfHeadingDAL());
+        public ActionResult Index()
+        {
+            var headingvalues = hm.GetList();
+            return View(headingvalues);
+        }
+    }
+}
