2e83276fc64ca4ea999a1a5a0702bef45da465ad
comlekahmetcan <77794522+comlekahmetcan@users.noreply.github.com>
2022-04-01 18:27:00 +0300

    SelectedListItem ile viewbag dropdownlist işlemi yapıldı(writer-category)-Başlıklarda Oluşturan Yazar Ve Kategori İşlemleri 48

--- MvcProjeKamp/Controllers/HeadingController.cs
+++ MvcProjeKamp/Controllers/HeadingController.cs
@@ -14,6 +14,7 @@ namespace MvcProjeKamp.Controllers
         // GET: Heading
         HeadingManager hm = new HeadingManager(new EfHeadingDAL());
         CategoryManager cm = new CategoryManager(new EfCategoryDAL());
+        WriterManager wm = new WriterManager(new EfWriterDAL());
         public ActionResult Index()
         {
             var headingvalues = hm.GetList();
@@ -22,14 +23,17 @@ namespace MvcProjeKamp.Controllers
         [HttpGet]
         public ActionResult AddHeading()
         {
-            List<SelectListItem> valuecategory = (from x in cm.GetList() select new SelectListItem {Text=x.CategoryName,Value=x.CategoryID.ToString()}).ToList();
+            List<SelectListItem> valuecategory = (from x in cm.GetList() select new SelectListItem { Text = x.CategoryName, Value = x.CategoryID.ToString() }).ToList();
             ViewBag.vlc = valuecategory;
+
+            List<SelectListItem> valuewriter = (from x in wm.GetList() select new SelectListItem { Text = x.WriterName + "" + x.WriterSurName, Value = x.WriterID.ToString() }).ToList();
+            ViewBag.vlw = valuewriter;
             return View();
         }
         [HttpPost]
         public ActionResult AddHeading(Heading p)
         {
-            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString()); 
+            p.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
             hm.HeadingAdd(p);
             return RedirectToAction("Index");
         }
